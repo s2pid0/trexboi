8292c523d239926125a840d54eae497e3d719384
s2pid0 <71849073+s2pid0@users.noreply.github.com>
2020-10-02 00:58:02 +0300

    про факториал

---
+++ Языки и методы программирования/02.10/fact.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, fact;
+  cin >> x; //ввод числа
+  fact = x; //присваиваем значение x факториалу, так как n!=1*2*3*...*n, т.е. мы перемножаем n с числами от 1 до n-1
+  for (int i=1; i < x; i++) { //циклом увеличиваем второе число в произведении, i<x не дает перемножить x на x
+    fact = x * i;
+  }
+  cout<< "Factorial of number " << x << " is " << fact << endl; //вывод
+  system("pause");
+  return(0);
+}
