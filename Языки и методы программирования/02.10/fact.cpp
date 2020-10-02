18f859b9bf0c73199c95e8b0d241d8540b45805c
s2pid0 <71849073+s2pid0@users.noreply.github.com>
2020-10-02 18:45:55 +0300

    Update fact.cpp

--- Языки и методы программирования/02.10/fact.cpp
+++ Языки и методы программирования/02.10/fact.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main() {
   int x, fact;
   cin >> x; //ввод числа
-  fact = x; //присваиваем значение x факториалу, так как n!=1*2*3*...*n, т.е. мы перемножаем n с числами от 1 до n-1
-  for (int i=1; i < x; i++) { //циклом увеличиваем второе число в произведении, i<x не дает перемножить x на x
+  fact = x; //присваиваем значение x факториалу
+  for (int i=1; i < x; i++) { 
     fact = fact * i;
   }
-  cout<< "Factorial of number " << x << " is " << fact << endl; //вывод
+  cout<< "Factorial of number " << x << " is " << fact << endl; 
   system("pause");
   return(0);
 }
