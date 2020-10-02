f2a6f6391df5cfa015de825dfb100d6ed074088c
s2pid0 <71849073+s2pid0@users.noreply.github.com>
2020-10-02 10:20:52 +0300

    Update seq.cpp

--- Языки и методы программирования/02.10/seq.cpp
+++ Языки и методы программирования/02.10/seq.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 using namespace std;
 
+int check(int num)
+{
+    for ( int i = 2; i < num; i++)
+    {
+        if (num % i == 0) { return 0;
+        } else
+        { 
+            return 1;
+        }
+    }
+}
+
+
+
 int main() {
   int x, fact;
-  cin >> x; //ввод числа-границы для нашей последовательности
-  cout<< "Your sequence is " << endl;
-  for (int i=0; i <= x; i++) { //выводим последовательность, если не знаешь как работает цикл фор то иди учи уроки а не пиво пей 
-    cout << i << " ";
+  cin >> x; 
+  cout << "Your sequence is " << endl;
+  for (int i=1; i <= x; i++) { 
+    if ( check (i) ) 
+    {
+        cout << i << " ";
+    }
   }
   cout << endl;
   system("pause");
