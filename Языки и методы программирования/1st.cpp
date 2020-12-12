8f8996523f88486ab6c2bd2d1585a64b1095c4c6
s2pid0 <71849073+s2pid0@users.noreply.github.com>
2020-12-12 13:35:57 +0300

    Create 1st.cpp

---
+++ Языки и методы программирования/1st.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int x = 0;
+
+ for (int i = 0; i < 15; i++) {
+     x = i;
+     switch (x) {
+         case 1:
+            cout << "Message #1" << endl;
+            break;
+         case 2:
+            cout << "Message #2" << endl;
+            break;
+         case 3:
+            cout << "Message #3" << endl;
+            break;
+         case 4:
+            cout << "Message #4" << endl;
+            break;
+         case 5:
+            cout << "Message #5" << endl;
+            break;
+         case 6:
+            cout << "Message #6" << endl;
+            break;
+         case 7:
+            cout << "Message #7" << endl;
+            break;
+         case 8:
+            cout << "Message #8" << endl;
+            break;
+         case 9:
+            cout << "Message #9" << endl;
+            break;
+         case 10:
+            cout << "Message #10" << endl;
+            break;
+         case 11:
+            cout << "Message #11" << endl;
+            break;
+         case 12:
+            cout << "Message #12" << endl;
+            break;
+         case 13:
+            cout << "Message #13" << endl;
+            break;
+         case 14:
+            cout << "Message #14" << endl;
+            break;
+         case 15:
+            cout << "Message #15" << endl;
+            break;
+     }
+ }
+
+  return(0);
+}
